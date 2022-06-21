03a729e1e9c655be95d736c40cc2424f8801065f
luishtcampos <luishtcampos7@gmail.com>
2022-06-21 18:55:40 -0300

    solucao plausivel

---
+++ fundamentos/bloco-4-javascript/dia-1-introducao-javascript/script3.js
@@ -0,0 +1,10 @@
+const notaDesafio = 89;
+if (notaDesafio >= 100) {
+    console.log("Parabéns, você foi aprovada(o)")
+}else if (notaDesafio => 80 || notaDesafio === 60) {
+    console.log("Você está na lista de espera");
+}else if (notaDesafio < 60) {
+        console.log("Você foi reprovada(o)");
+    } else {
+        console.log("Sua nota nem foi considerada");
+    }
